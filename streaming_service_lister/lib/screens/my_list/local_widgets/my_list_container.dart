e4ae3079c0591245085c5a6ac3513f8d4663613f
Sam <60146956+srudqvist@users.noreply.github.com>
2022-10-09 15:32:31 -0500

    MyList
    Moved the container for displaying the list into my_list.dart and added functionality for sorting the list and removing from the list

--- streaming_service_lister/lib/screens/my_list/local_widgets/my_list_container.dart
+++ streaming_service_lister/lib/screens/my_list/local_widgets/my_list_container.dart
@@ -1,120 +1,105 @@
-// service_selector.dart
-// Author: Samuel Rudqvist
-// Date Created: 09/26/2022
-//
+// // service_selector.dart
+// // Author: Samuel Rudqvist
+// // Date Created: 09/26/2022
+// //
 
-//********************************************************************//
-//*       This is the container for displaying the streaming         *//
-//*               services in the user's list.                       *//
-//********************************************************************//
+// //********************************************************************//
+// //*       This is the container for displaying the streaming         *//
+// //*               services in the user's list.                       *//
+// //********************************************************************//
 
 
-import 'package:flutter/material.dart';
-import 'package:streaming_service_lister/widgets/my_container.dart';
+// import 'package:flutter/material.dart';
+// import 'package:streaming_service_lister/widgets/my_container.dart';
+// import 'dart:developer' as developer;
 
-class MyListContainer extends StatefulWidget {
-  const MyListContainer({Key? key}) : super(key: key);
+// class MyListContainer extends StatefulWidget {
+//   const MyListContainer({Key? key}) : super(key: key);
 
-  @override
-  State<MyListContainer> createState() => _MyListContainerState();
-}
+//   @override
+//   State<MyListContainer> createState() => _MyListContainerState();
+// }
 
-class Movie{
-  String name;
-  int testInt;
+// class Movie{
+//   String name;
+//   int testInt;
 
-  Movie({required this.name, required this.testInt});
-}
-
-class TvShow{
-  String name;
-  int testInt;
-
-  TvShow({required this.name, required this.testInt});
-}
-
-class _MyListContainerState extends State<MyListContainer> {
+//   Movie({required this.name, required this.testInt});
   
-  List<String> myList = ["test", "test2","test5"];
-  List<Movie> movieList = [];
-  List<TvShow> tvShowList = [];
+// }
+
+// class TvShow{
+//   String name;
+//   int testInt;
+
+//   TvShow({required this.name, required this.testInt});
+//   //_myList.add(name);
+// }
+
+// class _MyListContainerState extends State<MyListContainer> {
   
-  @override
-  Widget build(BuildContext context) {
-    return MyContainer(
-      child: Column(
-        children: <Widget>[
+//   late String name;
+//   List<String> myList = ["test", "test2","test5"];
+//   List<Movie> movieList = [];
+//   List<TvShow> tvShowList = [];
+//   List<Widget> _myList = [];
+  
+//   @override
+//   Widget build(BuildContext context) {
+//     return MyContainer(
+//       child: Column(
+//         children: <Widget>[
           
-          Column(
-            children: <Widget>[
-              // Check if myList is empty 
-              myList.isEmpty? 
-                Text(
-                  "Add a Movie or Show",
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ):
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 8.0,),
-                  child: Text(
-                    "Your List",
-                    style: TextStyle(
-                      color: Theme.of(context).primaryColor,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                ListView.builder(
-                  padding: EdgeInsets.zero,
-                  shrinkWrap: true,
-                  itemCount: myList.length,
-                  // prototypeItem: ListTile(
-                  //   title: Text(myList.first)
-                  // ),
-                  itemBuilder: ((context, index) {
-                    return ListTile(
-                      //minVerticalPadding: 0.0,
-                      dense: true,
-                      visualDensity: const VisualDensity(vertical: 0.0),
-                      title: Text(
-                        myList[index],
-                        style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    );
-                  }),  
-                ),
-            ],
-          )
-          
-            
-            // ListView(
-            //       children: [
-            //         for (var item in myList) 
-            //         Text(
-            //           item, 
-            //           style: TextStyle(
-            //             color: Theme.of(context).primaryColor,
-            //             fontSize: 20.0,
-            //             fontWeight: FontWeight.bold,
-            //           ),
-            //         )
-            //       ],
-            //     ),
-                
-                
-              
-              
-          //),
-        ],
-      )
-    );
-  }
-}
+//           Column(
+//             children: <Widget>[
+//               // Check if myList is empty 
+//               _myList.isEmpty? 
+//                 Text(
+//                   "Add a Movie or Show",
+//                   style: TextStyle(
+//                     color: Theme.of(context).primaryColor,
+//                     fontSize: 20.0,
+//                     fontWeight: FontWeight.bold,
+//                   ),
+//                 ):
+//                 Padding(
+//                   padding: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 8.0,),
+//                   child: Text(
+//                     "Your List",
+//                     style: TextStyle(
+//                       color: Theme.of(context).primaryColor,
+//                       fontSize: 20.0,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                   ),
+//                 ),
+//                 ListView.builder(
+//                   padding: EdgeInsets.zero,
+//                   shrinkWrap: true,
+//                   itemCount: _myList.length,
+//                   // prototypeItem: ListTile(
+//                   //   title: Text(myList.first)
+//                   // ),
+//                   itemBuilder: ((context, index) {
+//                     return ListTile(
+//                       //minVerticalPadding: 0.0,
+//                       dense: true,
+//                       visualDensity: const VisualDensity(vertical: 0.0),
+//                       title: Text(
+//                         myList[index],
+//                         style: TextStyle(
+//                           color: Theme.of(context).primaryColor,
+//                           fontSize: 20.0,
+//                           fontWeight: FontWeight.bold,
+//                         ),
+//                       ),
+//                     );
+//                   }),  
+//                 ),
+//             ],
+//           )
+//         ],
+//       )
+//     );
+//   }
+// }
--- streaming_service_lister/lib/screens/my_list/my_list.dart
+++ streaming_service_lister/lib/screens/my_list/my_list.dart
@@ -11,6 +11,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:streaming_service_lister/screens/my_list/local_widgets/my_list_container.dart';
+import 'package:streaming_service_lister/widgets/my_container.dart';
+
+//import '../../widgets/my_container.dart';
 
 class MyList extends StatefulWidget {
   const MyList({Key? key}) : super(key: key);
@@ -21,6 +24,7 @@ class MyList extends StatefulWidget {
 
 class _MyListState extends State<MyList> {
   final myController = TextEditingController();
+  final List<String> myList = [];
 
   // @override
   // void dispose() {
@@ -76,7 +80,7 @@ class _MyListState extends State<MyList> {
                 ),
 
                 TextFormField(
-                  //controller: myController,
+                  controller: myController,
                   decoration: const InputDecoration(
                     prefixIcon: Icon(
                       Icons.search_outlined,
@@ -99,17 +103,89 @@ class _MyListState extends State<MyList> {
                       ),
                     ),
                   ),
-                  onPressed: (){}                                   
+                  onPressed: (){
+                    //print(myController.text);
+                    //print(myList);
+                    // Update the state of the widget when an item is added to the list
+                    setState(() {
+                      myList.add(myController.text);
+                      myList.sort();
+                      //key: UniqueKey();
+                    });
+                  }                                   
                 ),
-                
                 
                 const SizedBox(
                   height: 25.0,
                 ),
                 Row(
-                  children: const <Widget>[
+                  children: <Widget>[
                     Expanded(child: 
-                      MyListContainer(),
+                      //MyListContainer(),
+                      //MyContainer(child: Column()),
+                      MyContainer(
+                        child: Column(
+                          children: <Widget>[
+                            Column(
+                              children: <Widget>[
+                                // Check if myList is empty 
+                                myList.isEmpty? 
+                                  Text(
+                                    "Add a Movie or Show",
+                                    style: TextStyle(
+                                      color: Theme.of(context).primaryColor,
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ):
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 8.0,),
+                                    child: Text(
+                                      "Your List",
+                                      style: TextStyle(
+                                        color: Theme.of(context).primaryColor,
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  ListView.builder(
+                                    key: UniqueKey(),
+                                    padding: EdgeInsets.zero,
+                                    shrinkWrap: true,
+                                    itemCount: myList.length,
+                                    itemBuilder: ((context, index) {
+                                      return ListTile(
+                                        dense: true,
+                                        visualDensity: const VisualDensity(vertical: 0.0),
+                                        title: Text(
+                                          myList[index],
+                                          style: TextStyle(
+                                            color: Theme.of(context).primaryColor,
+                                            fontSize: 20.0,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        trailing: IconButton(
+                                          onPressed: (){
+                                            setState(() {
+                                              print("before");
+                                              print(myList);
+                                              myList.remove(myList[index]);
+                                              print("after");
+                                              print(myList);
+                                            });
+                                          },
+                                          icon: const Icon(Icons.delete_outline)
+                                        ),
+                                      );
+                                    }),  
+                                  ),
+                              ],
+                            )
+                          ],
+                        )
+                      )
                     )
                   ],
                 ),
