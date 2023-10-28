2607bd94a65d66e37efa05b7bd3c23c84e338753
Sam <60146956+srudqvist@users.noreply.github.com>
2023-10-28 15:47:36 -0500

    Styling on the options dialog

--- wheres_that_movie/lib/screens/suggestions/options_dialog.dart
+++ wheres_that_movie/lib/screens/suggestions/options_dialog.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class OptionsDialog extends StatelessWidget {
@@ -12,20 +13,72 @@ class OptionsDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // List options = [];
+    String alertTitle = "";
+    if (options[0] == "Netflix") {
+      alertTitle = "Choose a Streaming Service";
+    } else if (options[0] == "Comedy") {
+      alertTitle = "Choose a Genre";
+    }
+
     return AlertDialog(
-      title: const Text('Select an Option'),
+      backgroundColor: Theme.of(context).cardColor,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      // title: Text(alertTitle),
+      contentPadding: const EdgeInsets.all(0.0),
       content: Column(
         mainAxisSize: MainAxisSize.min,
-        children: options
-            .map((option) => ListTile(
-                  title: Text(option),
-                  onTap: () {
-                    onOptionSelected(option);
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                alignment: Alignment.topLeft,
+                // padding: const EdgeInsets.only(bottom: 10.0),
+                child: IconButton(
+                  visualDensity: VisualDensity.compact,
+                  splashRadius: 15,
+                  // splashColor: Colors.green,
+                  icon: Icon(
+                    CupertinoIcons.xmark_circle,
+                    color: Theme.of(context).primaryColor, // Specify the color
+                  ),
+                  onPressed: () {
                     Navigator.pop(context);
                   },
-                ))
-            .toList(),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+            child: Row(
+              children: [
+                Text(
+                  alertTitle,
+                  style: Theme.of(context).textTheme.displayMedium,
+                  textAlign: TextAlign.left,
+                ),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 8.0, right: 8.0),
+            child: Divider(
+              thickness: 5.0,
+              // color: Colors.red,
+            ),
+          ),
+
+          // Convert the iterable to a list of widgets
+          // Padding(padding: EdgeInsets.only(left: 8.0, right: 8.0, b))
+          ...options.map((option) => ListTile(
+                title: Text(option),
+                onTap: () {
+                  onOptionSelected(option);
+                  Navigator.pop(context);
+                },
+              )),
+        ],
       ),
     );
   }
