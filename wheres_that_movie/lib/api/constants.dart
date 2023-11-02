92617db982d46c4145ddb6d55eaf6dffc3a01873
Sam <60146956+srudqvist@users.noreply.github.com>
2023-11-02 14:31:07 -0500

    Added urls for suggestions to constants

--- wheres_that_movie/lib/api/constants.dart
+++ wheres_that_movie/lib/api/constants.dart
@@ -3,6 +3,61 @@
 //
 //
 
-class ApiConstants {
-  static String myTestEndpoint = "";
+class ApiEndPoint {
+  late String FILM_ALL;
+  late String FILM_SINGLE;
+  late String getMovieSuggestions;
+  late String getMovieGenresUrl;
+  late String getTvShowGenresUrl;
+  late String getMovieStreamingProviderInfo;
+  late String getMovieStreamingProviderInfoRegion;
+  late String getCountries;
+
+  // * Genre Docs: https://developer.themoviedb.org/reference/genre-movie-list
+
+  ApiEndPoint({int? id, List? suggestionsVariables}) {
+    //Value added for simplicity but it is always better
+    //  to add it in a configuration file
+    String baseUrlPath = 'https://api.themoviedb.org/3';
+
+    //   FILM_ALL = '$baseUrlPath/films/';
+    //   FILM_SINGLE = '$baseUrlPath/films/$id';
+
+    String region = "US";
+    String genreIDs = "35|53"; // Use pipe | for "or".
+    String providers = "8|9";
+
+    getMovieSuggestions =
+        '$baseUrlPath/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&watch_region=$region&with_genres=$genreIDs&with_watch_providers=$providers';
+
+    // discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&watch_region=US&with_genres=35&with_watch_providers=8
+
+    // Get the list of genres for movies or shows
+    getMovieGenresUrl = '$baseUrlPath/genre/movie/list?language=en';
+    getTvShowGenresUrl = '$baseUrlPath/genre/tv/list?language=en';
+
+    // Get the streaming provider information for movies
+    getMovieStreamingProviderInfo =
+        '$baseUrlPath/watch/providers/movie?language=en-US';
+    getMovieStreamingProviderInfoRegion =
+        '$baseUrlPath/watch/providers/movie?language=en-US&watch_region=US';
+
+    // Get the Countries used in TMDB
+    getCountries = '$baseUrlPath/configuration/countries?language=en-US';
+  }
 }
+
+// * Use like this
+// Future<List<Film>> getAllFilms() async {
+//   var response = await http.get(
+//     Uri.parse(ApiEndPoint().FILM_ALL),
+//   );
+// // ...
+// }
+
+// Future<Film> getSingleFilm(int idFilm) async {
+//   var response = await http.get(
+//     Uri.parse(ApiEndPoint(id: idFilm).FILM_SINGLE),
+//   );
+//   //...
+// }
--- wheres_that_movie/lib/screens/suggestions/suggestions.dart
+++ wheres_that_movie/lib/screens/suggestions/suggestions.dart
@@ -12,6 +12,51 @@
 // Modification Log:
 //    (xx/xx/xxxx)(SR):
 
+// GENRE CODES
+//https://www.themoviedb.org/talk/5daf6eb0ae36680011d7e6ee
+// MOVIE
+// Action          28
+// Adventure       12
+// Animation       16
+// Comedy          35
+// Crime           80
+// Documentary     99
+// Drama           18
+// Family          10751
+// Fantasy         14
+// History         36
+// Horror          27
+// Music           10402
+// Mystery         9648
+// Romance         10749
+// Science Fiction 878
+// TV Movie        10770
+// Thriller        53
+// War             10752
+// Western         37
+
+// TV SHOW
+// Action & Adventure  10759
+// Animation           16
+// Comedy              35
+// Crime               80
+// Documentary         99
+// Drama               18
+// Family              10751
+// Kids                10762
+// Mystery             9648
+// News                10763
+// Reality             10764
+// Sci-Fi & Fantasy    10765
+// Soap                10766
+// Talk                10767
+// War & Politics      10768
+// Western             37
+
+// ? https://levelup.gitconnected.com/how-i-organize-api-files-in-my-flutter-project-8f21c17050df
+
+// * Example: https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=en-US&page=1&sort_by=popularity.desc&watch_region=US&with_genres=35&with_watch_providers=8'
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:wheres_that_movie/screens/suggestions/options_dialog.dart';
@@ -83,9 +128,9 @@ class _SuggestionsState extends State<Suggestions> {
   loadUsers() async {
     final results = await UserService().getUser();
     print(results.length);
-    results.forEach((element) {
+    for (var element in results) {
       print(element.name.first);
-    });
+    }
   }
 
   @override
@@ -120,11 +165,11 @@ class _SuggestionsState extends State<Suggestions> {
         ),
         body: SafeArea(
             child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 20.0),
+          padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 20.0),
           child: Column(
             children: [
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Column(
                     children: [
@@ -133,10 +178,10 @@ class _SuggestionsState extends State<Suggestions> {
                           _showOptionsDialog(context, providers, setProviders);
                         },
                         style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                        ),
+                            minimumSize: const Size(175, 40),
+                            // maximumSize: const Size(200, 40),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20.0))),
                         child: Text(currentProvider),
                       ),
                       const SizedBox(
@@ -147,10 +192,10 @@ class _SuggestionsState extends State<Suggestions> {
                           _showOptionsDialog(context, genres, setGenre);
                         },
                         style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                        ),
+                            minimumSize: const Size(175, 40),
+                            // maximumSize: const Size(200, 40),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20.0))),
                         child: Text(currentGenre),
                       ),
                     ],
@@ -163,10 +208,10 @@ class _SuggestionsState extends State<Suggestions> {
                               context, movieOrShow, setMovieOrShow);
                         },
                         style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                        ),
+                            minimumSize: const Size(175, 40),
+                            // maximumSize: const Size(250, 40),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20.0))),
                         child: Text(currentMovieOrShow),
                       ),
                       const SizedBox(
@@ -177,10 +222,10 @@ class _SuggestionsState extends State<Suggestions> {
                           _showOptionsDialog(context, lengths, setLength);
                         },
                         style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                        ),
+                            minimumSize: const Size(175, 40),
+                            // maximumSize: const Size(250, 40),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20.0))),
                         child: Text(currentLength),
                       ),
                     ],
