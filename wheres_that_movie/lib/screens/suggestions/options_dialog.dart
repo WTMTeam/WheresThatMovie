8bedcefd9c06fbb54aa4bc5128f29d309221951e
Sam <60146956+srudqvist@users.noreply.github.com>
2023-11-11 20:23:30 -0600

    Added search to provider list in options dialog

--- wheres_that_movie/lib/screens/suggestions/options_dialog.dart
+++ wheres_that_movie/lib/screens/suggestions/options_dialog.dart
@@ -38,6 +38,22 @@ class _OptionsDialogState extends State<OptionsDialog> {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     // double screenHeight = MediaQuery.of(context).size.height;
+    TextEditingController searchController = TextEditingController();
+    List<Provider> filteredList = [];
+    String searchQuery = '';
+
+    Future<void> filterProviders(String query) async {
+      print("Query: $query");
+      List<Provider> providers = await futureProviders;
+      List<Provider> newProviders = [];
+      for (Provider provider in providers) {
+        if (provider.providerName.toLowerCase().contains(query.toLowerCase())) {
+          print(provider.providerName);
+          newProviders.add(provider);
+        }
+      }
+      filteredList = newProviders;
+    }
 
     String alertTitle = "";
     if (widget.button == "Provider") {
@@ -51,124 +67,218 @@ class _OptionsDialogState extends State<OptionsDialog> {
     }
 
     return AlertDialog(
+      // scrollable: true,
       backgroundColor: Theme.of(context).cardColor,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
       // title: Text(alertTitle),
       insetPadding: const EdgeInsets.all(0.0),
       contentPadding: const EdgeInsets.all(0.0),
-      content: SizedBox(
-        width: screenWidth / 1.2,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Align(
-              // heightFactor: 0.35,
-              alignment: Alignment.topRight,
-              // alignment: const Alignment(1.0, -1.0),
-              child: IconButton(
-                splashRadius: 10,
-                padding: const EdgeInsets.all(0.0),
-                icon: selectedProviders.isEmpty
-                    ? Icon(
-                        CupertinoIcons.xmark_circle,
-                        size: 30,
-                        color: Theme.of(context).primaryColor,
-                      )
-                    : Icon(
-                        CupertinoIcons.check_mark_circled,
-                        size: 30,
-                        color: Colors.green.shade800,
-                      ),
-                onPressed: () {
-                  if (widget.button == "Provider" &&
-                      selectedProviders.isNotEmpty) {
-                    widget.onOptionSelected(selectedProviders);
-                  }
+      content: StatefulBuilder(
+        builder: ((context, setState) {
+          double contentHeight = MediaQuery.of(context).size.height * 0.6;
+          return SingleChildScrollView(
+            child: SizedBox(
+              width: screenWidth / 1.2,
+              height: contentHeight,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Align(
+                    // heightFactor: 0.35,
+                    alignment: Alignment.topRight,
+                    // alignment: const Alignment(1.0, -1.0),
+                    child: IconButton(
+                      splashRadius: 10,
+                      padding: const EdgeInsets.all(0.0),
+                      icon: selectedProviders.isEmpty
+                          ? Icon(
+                              CupertinoIcons.xmark_circle,
+                              size: 30,
+                              color: Theme.of(context).primaryColor,
+                            )
+                          : Icon(
+                              CupertinoIcons.check_mark_circled,
+                              size: 30,
+                              color: Colors.green.shade800,
+                            ),
+                      onPressed: () {
+                        if (widget.button == "Provider" &&
+                            selectedProviders.isNotEmpty) {
+                          widget.onOptionSelected(selectedProviders);
+                        }
 
-                  Navigator.pop(context);
-                },
-              ),
-            ),
-
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Expanded(
-                  child: Text(
-                    alertTitle,
-                    style: Theme.of(context).textTheme.displayLarge,
-                    textAlign: TextAlign.center,
+                        Navigator.pop(context);
+                      },
+                    ),
                   ),
-                ),
-              ],
-            ),
 
-            const Padding(
-              padding: EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0),
-              child: Divider(
-                thickness: 5.0,
-                // color: Colors.red,
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Expanded(
+                        child: Text(
+                          alertTitle,
+                          style: Theme.of(context).textTheme.displayLarge,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const Padding(
+                    padding: EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0),
+                    child: Divider(
+                      thickness: 5.0,
+                      // color: Colors.red,
+                    ),
+                  ),
+
+                  // * If the Provider Button was pressed
+                  if (widget.button == "Provider")
+                    Expanded(
+                      child: Column(
+                        children: [
+                          TextField(
+                            controller: searchController,
+                            onChanged: (value) {
+                              setState(() {
+                                searchQuery = value;
+                                filterProviders(value);
+                              });
+                            },
+                            onEditingComplete: () {
+                              // print(searchController.text);
+                              // print("Search Query: $searchQuery");
+                              // setState(
+                              //   () {
+                              //     filterProviders(searchQuery);
+                              //   },
+                              // );
+                              // filterProviders(searchQuery);
+                              FocusScopeNode currentFocus =
+                                  FocusScope.of(context);
+                              currentFocus.unfocus();
+                            },
+                            decoration: InputDecoration(
+                              labelText: 'Search Providers',
+                              prefixIcon: Icon(Icons.search),
+                            ),
+                          ),
+                          Expanded(
+                            child: filteredList.isNotEmpty
+                                ? ListView.builder(
+                                    shrinkWrap: true,
+                                    itemCount: filteredList.length,
+                                    itemBuilder: ((context, index) {
+                                      Provider provider = filteredList[index];
+                                      String imgUrl =
+                                          "https://image.tmdb.org/t/p/w45${provider.logoPath}";
+                                      bool isSelected =
+                                          selectedProviders.contains(provider);
+                                      return ListTile(
+                                          leading: CachedNetworkImage(
+                                            imageUrl: imgUrl,
+                                            width: 50.0,
+                                            errorWidget: (context, imgUrl,
+                                                    error) =>
+                                                const Icon(
+                                                    Icons
+                                                        .no_photography_outlined,
+                                                    size: 50),
+                                          ),
+                                          title: Text(provider.providerName),
+                                          subtitle: Text(provider
+                                              .displayPriority
+                                              .toString()),
+                                          trailing: Checkbox(
+                                              value: isSelected,
+                                              onChanged: (value) {
+                                                setState(() {
+                                                  if (value != null) {
+                                                    if (value) {
+                                                      selectedProviders
+                                                          .add(provider);
+                                                    } else {
+                                                      selectedProviders
+                                                          .remove(provider);
+                                                    }
+                                                  }
+                                                });
+                                              }));
+                                    }))
+                                : FutureBuilder<List<Provider>>(
+                                    future: futureProviders,
+                                    builder:
+                                        ((context, AsyncSnapshot snapshot) {
+                                      if (snapshot.connectionState ==
+                                          ConnectionState.waiting) {
+                                        return CircularProgressIndicator();
+                                      } else if (snapshot.hasError) {
+                                        return Text("Error: ${snapshot.error}");
+                                      } else {
+                                        print(
+                                            "Snapshot: ${snapshot.data.length}");
+                                        return ListView.builder(
+                                          // physics: const NeverScrollableScrollPhysics(),
+                                          shrinkWrap: true,
+                                          itemBuilder: (context, index) {
+                                            Provider provider =
+                                                snapshot.data?[index];
+
+                                            String imgUrl =
+                                                "https://image.tmdb.org/t/p/w45${provider.logoPath}";
+                                            bool isSelected = selectedProviders
+                                                .contains(provider);
+                                            return ListTile(
+                                                leading: CachedNetworkImage(
+                                                  imageUrl: imgUrl,
+                                                  width: 50.0,
+                                                  errorWidget: (context, imgUrl,
+                                                          error) =>
+                                                      const Icon(
+                                                          Icons
+                                                              .no_photography_outlined,
+                                                          size: 50),
+                                                ),
+                                                title:
+                                                    Text(provider.providerName),
+                                                subtitle: Text(provider
+                                                    .displayPriority
+                                                    .toString()),
+                                                trailing: Checkbox(
+                                                    value: isSelected,
+                                                    onChanged: (value) {
+                                                      setState(() {
+                                                        if (value != null) {
+                                                          if (value) {
+                                                            selectedProviders
+                                                                .add(provider);
+                                                          } else {
+                                                            selectedProviders
+                                                                .remove(
+                                                                    provider);
+                                                          }
+                                                        }
+                                                      });
+                                                    }));
+                                          },
+
+                                          itemCount: snapshot.data.length,
+                                        );
+                                      }
+                                    }),
+                                  ),
+                          ),
+                        ],
+                      ),
+                    )
+                  else
+                    const Text("test")
+                ],
               ),
             ),
-
-            // * If the Provider Button was pressed
-            (widget.button == "Provider")
-                ? FutureBuilder<List<Provider>>(
-                    future: futureProviders,
-                    builder: ((context, AsyncSnapshot snapshot) {
-                      if (snapshot.hasData) {
-                        return Expanded(
-                          child: ListView.separated(
-                              itemBuilder: (context, index) {
-                                Provider provider = snapshot.data?[index];
-                                String imgUrl =
-                                    "https://image.tmdb.org/t/p/w45${provider.logoPath}";
-                                bool isSelected =
-                                    selectedProviders.contains(provider);
-                                return ListTile(
-                                    leading: CachedNetworkImage(
-                                      imageUrl: imgUrl,
-                                      width: 50.0,
-                                      errorWidget: (context, imgUrl, error) =>
-                                          const Icon(
-                                              Icons.no_photography_outlined,
-                                              size: 50),
-                                    ),
-                                    title: Text(provider.providerName),
-                                    subtitle: Text(
-                                        provider.displayPriority.toString()),
-                                    trailing: Checkbox(
-                                        value: isSelected,
-                                        onChanged: (value) {
-                                          setState(() {
-                                            if (value != null) {
-                                              if (value) {
-                                                selectedProviders.add(provider);
-                                              } else {
-                                                selectedProviders
-                                                    .remove(provider);
-                                              }
-                                            }
-                                          });
-                                        }));
-                              },
-                              separatorBuilder: (context, index) {
-                                return const Divider(
-                                  color: Colors.amberAccent,
-                                );
-                              },
-                              itemCount: 20),
-                        );
-                        // itemCount: snapshot.data!.length);
-                      } else if (snapshot.hasError) {
-                        return Text("Error: ${snapshot.error}");
-                      }
-                      return const CircularProgressIndicator();
-                    }),
-                  )
-                : const Text("test")
-          ],
-        ),
+          );
+        }),
       ),
     );
   }
