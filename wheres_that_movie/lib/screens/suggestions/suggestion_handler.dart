e7e31b0beb044066f5342dda77d07f54e045e423
Sam <60146956+srudqvist@users.noreply.github.com>
2024-01-16 21:38:46 -0600

    Started suggestions function

--- wheres_that_movie/lib/screens/suggestions/suggestion_handler.dart
+++ wheres_that_movie/lib/screens/suggestions/suggestion_handler.dart
@@ -1,6 +1,42 @@
+import 'package:wheres_that_movie/api/models/genre_model.dart';
+import 'package:wheres_that_movie/api/models/provider_model.dart';
 
-
+// Create a function to return the nesessary data
+// for displaying one or multiple movies
+//
+// This function takes in the variables from suggestions.dart
+// when the user taps the suggestion button
+//
+//
 class SuggestionsHandler {
-  // Create a function to return the nesessary data 
-  // for displaying one or multiple movies
+  final List<Provider> providers;
+  final List<Genre> genres;
+  final int runningTime;
+  final String movieOrShow;
+
+  SuggestionsHandler({
+    required this.providers,
+    required this.genres,
+    required this.runningTime,
+    required this.movieOrShow,
+  });
+
+  String buildUrlString(dynamic urlVariable) {
+    String urlString = "";
+
+    return urlString;
+  }
+
+// url to be used?
+//'$baseUrlPath/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&watch_region=$region&with_genres=$genreIDs&with_watch_providers=$providers';
+
+  SuggestionsHandler getSuggestion(List<Provider> providers, List<Genre> genres,
+      int runningTime, String movieOrShow) {
+    // Your function logic here
+    return SuggestionsHandler(
+        providers: providers,
+        genres: genres,
+        runningTime: runningTime,
+        movieOrShow: movieOrShow);
+  }
 }
